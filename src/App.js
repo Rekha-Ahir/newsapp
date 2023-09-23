79f428772f75c92ce95fe1b66bb8220e6c5c66da
Rekha-Ahir <REKHAAHIR2002@GMAIL.COM>
2023-09-23 03:09:37 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+import React, { useState } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+
+import LoadingBar from 'react-top-loading-bar'
+// import  from 'react-router-dom/cjs/react-router-dom.min';
+// import * as ReactDOM from "react-dom/client";
+
+const App = ()=> {
+  const pageSize = 5;
+  // const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+ 
+    return (
+      <div>
+        <BrowserRouter>
+        <NavBar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress} 
+      />
+         <Routes>
+          <Route  exact path="/general" element ={<News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/>}/> 
+          <Route  path="/business" element ={<News setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="business"/>}/> 
+          <Route  path="/entertainment" element ={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/> 
+          <Route  path="/general" element ={<News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/>}/> 
+          <Route  path="/health" element ={<News setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="health"/>}/> 
+          <Route  path="/science" element ={<News setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="science"/>}/> 
+          <Route  path="/sports" element ={<News setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="sports"/>}/> 
+          <Route  path="/technology" element ={<News setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="technology"/>}/> 
+          </Routes>
+          </BrowserRouter>
+      </div>
+    )
+ 
+}
+
+export default App;
